15ecb8a0b8e4782c68dca359696a7764701fdb39
Robert Holt <robert.holt@posmc.com.au>
2016-04-09 11:32:31 +1000

    Added a script to assemble bean submissions

---
+++ collate.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+BEANFLD="beanCore"
+BEANZIP="bean.zip"
+
+BEANFILES="bean_ast.ml \
+           bean_ast.mli \
+           bean_lex.mll \
+           bean_parse.mly \
+           bean_pprint.ml \
+           bean_pprint.mli \
+           Makefile \
+           Makefile.depend"
+
+
+if [ -e $BEANFLD ]; then
+  rm -rf $BEANFLD
+fi
+
+if [ -f $BEANZIP ]; then
+  rm -f $BEANZIP
+fi
+
+mkdir $BEANFLD
+
+for file in $BEANFILES; do
+  cp $file $BEANFLD
+done
+
+zip -r -o $BEANZIP $BEANFLD
